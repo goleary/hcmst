1f3df692ff8a127fd80ff24cab11defb8fc7d691
Gabe O'Leary <oleary.gabe@gmail.com>
2021-03-21 19:26:22 -0700

    convert api route to TS

--- web/pages/api/predict.ts
+++ web/pages/api/predict.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { NextApiHandler } from "next";
+
 const API_BASE_URL = "https://plumber-ldldwxmigq-uw.a.run.app/";
 
 const PREDICT_ROUTE = "predict";
@@ -8,11 +10,17 @@ const PREDICT_ROUTE = "predict";
 // is there an easier way to do with with next API routes?
 // this also means we could turn authentication on and set it up here.
 
-export default async (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
   try {
+    const { income, ageBin, education } = req.query as {
+      [key: string]: string;
+    };
+
     const url = new URL(PREDICT_ROUTE, API_BASE_URL);
-    url.search = new URLSearchParams(req.query);
-    const result = await fetch(url);
+    url.searchParams.append("income", income);
+    url.searchParams.append("education", education);
+    url.searchParams.append("age_bin", ageBin);
+    const result = await fetch(url.toString());
     const text = await result.text();
     console.log({ text });
     res.send(text);
@@ -21,3 +29,5 @@ export default async (req, res) => {
     res.status(500).json({ error });
   }
 };
+
+export default handler;
