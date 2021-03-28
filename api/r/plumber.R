dec8e1a23c78bf6ca60f4ad4f80f113d3e525758
djolear <djolear@users.noreply.github.com>
2021-03-28 14:25:35 -0700

    return json object

--- api/r/plumber.R
+++ api/r/plumber.R
@@ -6,27 +6,41 @@ library(plumber)
 #* @param education
 #* @param age_bin 
 #* @get /predict
+#* @serializer unboxedJSON
+
 
 marital_status_predict <- function(income, education, age_bin){
   
   load('./model.RData')
   
-  test <- 
-    c(income, education, age_bin)
+  #test = c("52000", "8", "2")
   
+  test = c(income, education, age_bin)
   test = sapply(test, as.numeric)
   test = data.frame(matrix(test, ncol = 3))
   
   
-  colnames(test) <- 
-    c("income", "education", "age_bin")
+  colnames(test) <- c("income", "education", "age_bin")
   
-
+  test <- as.matrix(test)
   
-  test <-
-    as.matrix(test)
+  probs <- predict(xgb.fit, test)
   
-  predict(xgb.fit, test)
+  levels <- c("married", "widowed", "divorced", "separated", "never married", "living with your partner")
+  
+  results <- data.frame(levels, probs)
+  
+  results <- 
+    list(
+      "married" = results$probs[1],
+      "widowed" = results$probs[2],
+      "divorced" = results$probs[3],
+      "separated" = results$probs[4],
+      "never_married" = results$probs[5],
+      "living_with_your_partner" = results$probs[6]
+    )
+  
+  return(results)
   
 }
 
